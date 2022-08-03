5ac63db1421c329208502e08caef9f2c5a9c0bf7
edward-vera <103536879+edward-vera@users.noreply.github.com>
2022-08-02 20:14:28 -0500

    final commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// .map() - function that takes an array of items and a function that returns an array with each item manipulated by that function.
+const arr = [4, 9, 16, 25, 36];
+const newArr = arr.map(Math.sqrt);
+console.log(newArr);
+
+// .reduce() - function that takes an array of food orders with their amounts and returns the total amount of all the food orders.
+const price = [{item: "turkey", cost: 69.69}, {item: "pizza", cost: 420}, {item: "beans", cost: 16}];
+const i = 0; 
+
+const value = price.map(money => money.cost)
+
+const sumTotal = value.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    i
+)
+
+console.log(sumTotal);
+
+
+// .filter() - function that takes an array of items and a function that returns an array with only the items that return true in the function.
+
+const items = ['penny', 'maze', 'iguana', 'alamo', 'bowl'];
+
+const results = items.filter(item => item.length > 4);
+
+console.log(results);
