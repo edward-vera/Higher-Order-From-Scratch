b332c70e7c932a15b0d3bd23cf747ab78fd4324d
edward-vera <103536879+edward-vera@users.noreply.github.com>
2022-08-02 20:16:39 -0500

    added more to the clg

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // .map() - function that takes an array of items and a function that returns an array with each item manipulated by that function.
 const arr = [4, 9, 16, 25, 36];
 const newArr = arr.map(Math.sqrt);
-console.log(newArr);
+console.log("The square root of each of the numbers are" , newArr);
 
 // .reduce() - function that takes an array of food orders with their amounts and returns the total amount of all the food orders.
 const price = [{item: "turkey", cost: 69.69}, {item: "pizza", cost: 420}, {item: "beans", cost: 16}];
@@ -14,7 +14,7 @@ const sumTotal = value.reduce(
     i
 )
 
-console.log(sumTotal);
+console.log("The total of all items purchased is" , sumTotal);
 
 
 // .filter() - function that takes an array of items and a function that returns an array with only the items that return true in the function.
@@ -23,4 +23,4 @@ const items = ['penny', 'maze', 'iguana', 'alamo', 'bowl'];
 
 const results = items.filter(item => item.length > 4);
 
-console.log(results);
+console.log("Items with more than 4 letters" , results);
